d5b839ac8a3366cebebbe1ebebbf26d023259b79
xcnwf <xcnwf@prontonmail.com>
2025-04-22 18:07:10 +0200

    feat: made buff infinite (& removed unused attributes)

--- Content/Buffs/Captured.cs
+++ Content/Buffs/Captured.cs
@@ -12,32 +12,35 @@ namespace Monstermon.Content.Buffs
         {
             // Display name and tooltip shown when hovering over buff icon
             // Localization is now handled through language files
-            
+
             // This buff is not saved when the world is exited
-            Main.buffNoTimeDisplay[Type] = true;
             Main.buffNoSave[Type] = true;
-            
+
+            // Do not display remaining time
+            Main.buffNoTimeDisplay[Type] = true;
+
             // This buff is bad for NPCs (marks them for capture)
             Main.debuff[Type] = true;
-            
-            // Make sure the buff doesn't get removed by exiting the game
-            Main.persistentBuff[Type] = true;
-            
+
             // Don't allow this buff to be blocked by NPCs
             BuffID.Sets.NurseCannotRemoveDebuff[Type] = true;
         }
-        
+
         public override void Update(NPC npc, ref int buffIndex)
         {
             // Visual effect to show the NPC is marked for capture
             if (Main.rand.NextBool(5))
             {
-                Vector2 position = npc.Center + new Vector2(Main.rand.Next(-npc.width/2, npc.width/2), Main.rand.Next(-npc.height/2, npc.height/2));
+                Vector2 position = npc.Center + new Vector2(Main.rand.Next(-npc.width / 2, npc.width / 2), Main.rand.Next(-npc.height / 2, npc.height / 2));
                 Dust.NewDust(position, 1, 1, DustID.MagicMirror, 0f, 0f, 0, Color.Cyan, 0.8f);
             }
+
+            // Increase duration if buff was to expire
+            if (npc.buffTime[buffIndex] < 60)
+                npc.buffTime[buffIndex] += 600;
         }
 
-        public override void ModifyBuffText(ref string buffName, ref string tip, ref int rare) 
+        public override void ModifyBuffText(ref string buffName, ref string tip, ref int rare)
         {
             buffName = "Captured!";
             tip = "This mob has been captured by a player.";
